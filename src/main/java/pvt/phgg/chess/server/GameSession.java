bbe863890b389947cb0667525657e9f67a3e3beb
rooimoer47 <phggrabe@gmail.com>
2026-06-21 21:31:07 +0200

    Upgrade to Spring Boot 4.0.7 and Jackson 3 (tools.jackson.databind)

--- src/main/java/pvt/phgg/chess/server/GameSession.java
+++ src/main/java/pvt/phgg/chess/server/GameSession.java
@@ -1,6 +1,6 @@
 package pvt.phgg.chess.server;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import tools.jackson.databind.ObjectMapper;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import pvt.phgg.chess.*;
--- src/test/java/pvt/phgg/chess/server/GameSessionTest.java
+++ src/test/java/pvt/phgg/chess/server/GameSessionTest.java
@@ -1,6 +1,6 @@
 package pvt.phgg.chess.server;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import tools.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpHeaders;
